9413494c7d90693da2c54651cfd841f5354c7f44
Jin Hu, Baek <m@ake.kr>
2014-08-01 03:53:11 +0900

    Added SimplePagination Library
    파일추가했슴

---
+++ Simplepagination.php
@@ -0,0 +1,50 @@
+<?php
+	class Simplepagination{
+		function __construct(){
+
+		}
+
+		function getPagination($configure = false){
+			if(is_array($configure) && ($configure['last_page'] >= $configure['current_page'])) {
+
+				$configure['last_page'] = (int)$configure['last_page'];
+
+				$html_return_string = '<ul class="core pagination">';
+				$page = (int)(($configure['current_page']-1) / 10)*10;
+
+				if(($page != 0) && ($configure['last_page'] > 10)){
+					if(($configure['last_page'] > 20) && ($configure['current_page'] > 20)) {
+						$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/1') . '" class="firstpage">&laquo;</a></li>';
+					}
+					$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . ($page-9) ) . '" class="prevpage">&lt;</a></li>';
+				}
+
+				$loop_page_count = (($configure['last_page'] - $page) >= 10) ? 10 : $configure['last_page']-$page;
+
+				for($i=$page+1; $i<=$page+$loop_page_count; $i++){
+					if($i == $configure['current_page']){
+						$html_return_string .= '<li><span class="active">' . $i . '</span></li>';
+					} else {
+						$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . $i) . '">' . $i . '</a></li>';
+					}
+				}
+
+				if(($configure['last_page'] > 10) && ( $page+10 < $configure['last_page'])) {
+					$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . ($page+11)) . '" class="nextpage">&gt;</a></li>';
+					if(($configure['last_page'] - $page) > 20){
+						if(is_int($configure['last_page'] / 10)) {
+							$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . (((int)($configure['last_page'] / 10))*10)) . '" class="lastpage">&raquo;</a></li>';
+						} else {
+							$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . (((int)($configure['last_page'] / 10))*10 + 1)) . '" class="lastpage">&raquo;</a></li>';
+						}
+					}
+				}
+
+				$html_return_string .= '</ul>';
+
+				return $html_return_string;
+			} else {
+				return false;
+			}
+		}
+	}
