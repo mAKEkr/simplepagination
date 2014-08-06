818825b22ca2145f86439d9e1d2c779f93eac12a
Jin Hu, Baek <m@ake.kr>
2014-08-06 15:54:45 +0900

    Added suffix code.
    사실 넣는거 깜빡함

--- Simplepagination.php
+++ Simplepagination.php
@@ -14,9 +14,9 @@
 
 				if(($page != 0) && ($configure['last_page'] > 10)){
 					if(($configure['last_page'] > 20) && ($configure['current_page'] > 20)) {
-						$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/1') . '" class="firstpage">&laquo;</a></li>';
+						$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/1' . $configure['suffix']) . '" class="firstpage">&laquo;</a></li>';
 					}
-					$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . ($page-9) ) . '" class="prevpage">&lt;</a></li>';
+					$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . ($page-9) . $configure['suffix']) . '" class="prevpage">&lt;</a></li>';
 				}
 
 				$loop_page_count = (($configure['last_page'] - $page) >= 10) ? 10 : $configure['last_page']-$page;
@@ -25,17 +25,17 @@
 					if($i == $configure['current_page']){
 						$html_return_string .= '<li><span class="active">' . $i . '</span></li>';
 					} else {
-						$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . $i) . '">' . $i . '</a></li>';
+						$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . $i . $configure['suffix']) . '">' . $i . '</a></li>';
 					}
 				}
 
 				if(($configure['last_page'] > 10) && ( $page+10 < $configure['last_page'])) {
-					$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . ($page+11)) . '" class="nextpage">&gt;</a></li>';
+					$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . ($page+11) . $configure['suffix']) . '" class="nextpage">&gt;</a></li>';
 					if(($configure['last_page'] - $page) > 20){
 						if(is_int($configure['last_page'] / 10)) {
-							$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . (((int)($configure['last_page'] / 10))*10)) . '" class="lastpage">&raquo;</a></li>';
+							$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . (((int)($configure['last_page'] / 10))*10) . $configure['suffix']) . '" class="lastpage">&raquo;</a></li>';
 						} else {
-							$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . (((int)($configure['last_page'] / 10))*10 + 1)) . '" class="lastpage">&raquo;</a></li>';
+							$html_return_string .= '<li><a href="' . site_url($configure['url'] . '/' . (((int)($configure['last_page'] / 10))*10 + 1) . $configure['suffix']) . '" class="lastpage">&raquo;</a></li>';
 						}
 					}
 				}
